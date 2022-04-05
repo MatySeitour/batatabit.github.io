b54c084f5b65efc6326723364275978d13161c28
Matias Seitour <matias.seitour01@gmail.com>
2022-04-05 18:34:33 -0300

    some more effects

--- efects.js
+++ efects.js
@@ -1,9 +1,29 @@
+// LOGO EFECTS
+const logo = document.getElementById("logo-batata");
+let sizeScreen = window.innerHeight / 1
+let position = logo.getBoundingClientRect().top;
+
+window.onload = function(){
+        logo.classList.add("logo-effect");
+}
+
+// function appear(){
+//     if(){
+//         logo.classList.add("logo-effect")
+//         console.log(1);
+//     }
+// }
+
+// window.addEventListener("scroll", appear);
+// TABLE EFECTS
+
 const buton_right = document.getElementById("buton-right");
 const buton_left = document.getElementById("buton-left");
 const right = document.querySelector(".right-arrow-tables");
 const left = document.querySelector(".left-arrow-tables");
 const tableCoins = document.querySelector(".table-container")
 const tableComision = document.querySelector(".table-container2");
+const mainDate = document.querySelector(".main-date");
 left.classList.remove("left-bye");
 
 
@@ -22,17 +42,28 @@ buton_left.addEventListener("click", () => {
 
 })
 
-const fecha = () =>{
-    return new Promise((resolve, reject) =>{
-        if(true){
-            setTimeout((resolve) => {
-                resolve = "Actualizado: "
-                console.log(resolve + new Date())
-            }, 1000)
-        }
-    })
-}
+// DATE TABLE
 
-fecha().then(resolve => console.log(resolve));
+const date = new Date().toString();
 
-const rere = "hola";
+const dateArr = date.split(" ");
+
+mainDate.innerHTML = "<b>Actualizado : </b>" + dateArr[2] + " " + dateArr[1] + " " + dateArr[4];
+
+
+// const fecha = () =>{
+//     return new Promise((resolve, reject) =>{
+//         if(true){
+//             setTimeout((resolve) => {
+//                 resolve = "Actualizado: ";
+//                 let actually = new Date().toString();
+//                 console.log(actually.split(" "));
+//             }, 1000)
+//         }
+//     })
+// }
+
+// fecha().then(resolve => console.log(resolve));
+
+
+
