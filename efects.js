965e270a56f5c6c3fcd3a597919c1aa167035bda
Matias Seitour <matias.seitour01@gmail.com>
2022-04-07 10:54:05 -0300

    some more effects about plan section

--- efects.js
+++ efects.js
@@ -7,16 +7,6 @@ window.onload = function(){
         logo.classList.add("logo-effect");
 }
 
-// function appear(){
-//     if(){
-//         logo.classList.add("logo-effect")
-//         console.log(1);
-//     }
-// }
-
-// window.addEventListener("scroll", appear);
-// TABLE EFECTS
-
 const buton_right = document.getElementById("buton-right");
 const buton_left = document.getElementById("buton-left");
 const right = document.querySelector(".right-arrow-tables");
@@ -24,6 +14,7 @@ const left = document.querySelector(".left-arrow-tables");
 const tableCoins = document.querySelector(".table-container")
 const tableComision = document.querySelector(".table-container2");
 const mainDate = document.querySelector(".main-date");
+const mainDate2 = document.querySelector(".main-date2");
 left.classList.remove("left-bye");
 
 
@@ -42,7 +33,7 @@ buton_left.addEventListener("click", () => {
 
 })
 
-// DATE TABLE
+// DATE TABLE 1
 
 const date = new Date().toString();
 
@@ -50,20 +41,37 @@ const dateArr = date.split(" ");
 
 mainDate.innerHTML = "<b>Actualizado : </b>" + dateArr[2] + " " + dateArr[1] + " " + dateArr[4];
 
+// DATE TABLE 2
 
-// const fecha = () =>{
-//     return new Promise((resolve, reject) =>{
-//         if(true){
-//             setTimeout((resolve) => {
-//                 resolve = "Actualizado: ";
-//                 let actually = new Date().toString();
-//                 console.log(actually.split(" "));
-//             }, 1000)
-//         }
-//     })
-// }
+const date2 = new Date().toString();
 
-// fecha().then(resolve => console.log(resolve));
+const dateArr2 = date.split(" ");
+
+mainDate2.innerHTML = "<b>Actualizado : </b>" + dateArr[2] + " " + dateArr[1] + " " + dateArr[4];
+
+// EFFECTS PLANS
+
+const yearplan = document.querySelector(".cards-plan");
+const monthlyplan = document.querySelector(".cards-plan2");
+const arrowLeftPlan = document.querySelector(".arrow-left__plan");
+const arrowRightPlan = document.querySelector(".arrow-right__plan");
+arrowLeftPlan.style.display="none";
+
+yearplan.classList.remove("card1-show");
+yearplan.classList.remove("card1-hide");
+monthlyplan.classList.remove("card2-show");
+monthlyplan.classList.remove("card2-hide");
 
 
+arrowRightPlan.onclick = () =>{
+    yearplan.classList.add("card1-show");
+    monthlyplan.classList.add("card2-show");
+    arrowRightPlan.style.display="none";
+    arrowLeftPlan.style.display="inline-block";
+}
 
+arrowLeftPlan.onclick = () =>{
+    yearplan.classList.remove("card1-show");
+    monthlyplan.classList.remove("card2-show");
+    yearplan.classList.add("card1-hide");
+}
